e7934b6bd074427ca4c9d2296470efd3166bb588
Rakib Islam <109210735+rakib-islam98@users.noreply.github.com>
2026-02-15 11:39:58 +0530

    Enhance comments for clarity in app.py

--- diabetes/app.py
+++ diabetes/app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load model and scaler
+
+# Loading model and scaler
 model = joblib.load("models/logistic_balanced_model.pkl")
 scaler = joblib.load("models/scaler.pkl")
 
@@ -11,10 +12,8 @@ st.set_page_config(page_title="Diabetes Risk Prediction")
 st.title("🩺 Diabetes Risk Prediction System")
 st.write("Enter patient details below to assess diabetes risk.")
 
-# -------------------------
-# User Inputs
-# -------------------------
 
+# Taking user input
 preg = st.number_input("Pregnancies", min_value=0, step=1)
 glucose = st.number_input("Glucose Level (mg/dL)")
 bp = st.number_input("Blood Pressure (mm Hg)")
@@ -23,13 +22,15 @@ insulin = st.number_input("Insulin Level (µU/mL)")
 bmi = st.number_input("BMI")
 age = st.number_input("Age", min_value=1, step=1)
 
-# Family History instead of DPF
+
+# Checking family history for DPF
 family_history = st.selectbox(
     "Family History of Diabetes",
     ["None", "One Parent", "Both Parents", "Siblings"]
 )
 
-# Map family history to approximate DPF value
+
+# Mapping family history to approximate DPF value
 if family_history == "None":
     dpf = 0.2
 elif family_history == "One Parent":
@@ -39,17 +40,12 @@ elif family_history == "Both Parents":
 else:
     dpf = 1.0
 
-# -------------------------
+
 # Prediction Button
-# -------------------------
-
 if st.button("Predict"):
-
-    # Apply log transform to insulin (same as training)
+    # log transform to insulin
     insulin = np.log1p(insulin)
-
     input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
-
     input_scaled = scaler.transform(input_data)
 
     prediction = model.predict(input_scaled)[0]
